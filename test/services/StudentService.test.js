1aa48ae99433d2a2c20917fe21b42872a9ffce96
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-10 23:44:13 -0500

    Add getStudentsCreditsGreater tes

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -6,4 +6,9 @@ describe("Unit test for StudentService", () =>{
         const emailStudents = StudentService.getEmailsCertifiedStudents(students);
         expect(emailStudents.length).toBe(1);
     });
+    test("2. Get all students with credits greater than 500", () =>{
+        const students = [{credits: 200}, {credits:600}, {credits: 501}];
+        const studentsGreater = StudentService.getStudentsCreditsGreater(students, 500);
+        expect(studentsGreater.length).toBe(2);
+    });
 });
