58288024ae4ed25d88285a5ab877c8d9c7a82ad1
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-10 22:47:10 -0500

    Add get certified students email test

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -3,7 +3,7 @@ const StudentService = require("./../../lib/services/StudentService")
 describe("Unit test for StudentService", () =>{
     test("1. Get emails certified students", () =>{
         const students = [{haveCertification: true, email: 'certifiedstudent@gmail.com'}, {haveCertification: false, email: 'nocertifiedstudent@gmail.com'}]
-        const emailStudents = StudentService(students);
+        const emailStudents = StudentService.getEmailsCertifiedStudents(students);
         expect(emailStudents.length).toBe(1);
     });
 });
