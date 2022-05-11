acb6ba1349cb3816d20e3a30e6a23173523ffc02
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-10 23:00:52 -0500

    Update

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,8 +1,12 @@
-const StudentContoller = require('../../lib/controllers/StudentController')
+const StudentContoller = require("../../lib/controllers/StudentController");
 
 describe("Unit test for StrudentController", () =>{
     test("1. Get all students", () =>{
         const allStudents = StudentContoller.getAllStudents();
         expect(allStudents.length).toBe(51);
+    });
+    test("2. Get certified students emails", () =>{
+        const emailStudents = StudentContoller.getEmailsCertifiedStudents();
+        expect(emailStudents.length).toBe(29);
     });
 });
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,8 +1,8 @@
-const StudentService = require("./../../lib/services/StudentService")
+const StudentService = require("./../../lib/services/StudentService");
 
 describe("Unit test for StudentService", () =>{
     test("1. Get emails certified students", () =>{
-        const students = [{haveCertification: true, email: 'certifiedstudent@gmail.com'}, {haveCertification: false, email: 'nocertifiedstudent@gmail.com'}]
+        const students = [{haveCertification: true, email: "certifiedstudent@gmail.com"}, {haveCertification: false, email: "nocertifiedstudent@gmail.com"}];
         const emailStudents = StudentService.getEmailsCertifiedStudents(students);
         expect(emailStudents.length).toBe(1);
     });
