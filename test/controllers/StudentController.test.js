59002fb5a8eabc86a3e863cc1a55712db4ee00a8
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-10 22:13:01 -0500

    Add test requirement 1

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,6 +1,8 @@
+const StudentContoller = require('../../lib/controllers/StudentController')
+
 describe("Unit test for StrudentController", () =>{
     test("1. Get all students", () =>{
         const allStudents = StudentContoller.getAllStudents();
-        expect(allStudents.length).toBe(1);
+        expect(allStudents.length).toBe(51);
     });
 });
